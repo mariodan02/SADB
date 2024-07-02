7a5eb532ea9a28241544325428da1dfea986c274
Studente <studente@MacBook-Air-di-Dipartimento-4.local>
2024-07-02 16:49:10 +0200

    First commit

---
+++ sadb/sadb/sadbApp.swift
@@ -0,0 +1,17 @@
+//
+//  sadbApp.swift
+//  sadb
+//
+//  Created by Studente on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct sadbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
