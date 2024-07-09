390e208975d567633d9aa8f47354552c31d48f36
merola02 <a.merola31@studenti.unisa.it>
2024-07-09 11:58:02 +0200

    f

--- sadb/sadb/goalsView.swift
+++ sadb/sadb/goalsView.swift
@@ -11,15 +11,23 @@ struct GoalsView: View {
     var body: some View {
         NavigationStack{
             VStack {
-                
                 List {
-                    ProgressRow(imageName: "clock", text: "Non hai fumato sigarette per 1 giorno intero", achieved: true)
-                    ProgressRow(imageName: "clock", text: "Non hai fumato sigarette per 3 giorni", achieved: true)
-                    ProgressRow(imageName: "clock", text: "Non hai fumato sigarette per 1 settimana", achieved: true)
-                    ProgressRow(imageName: "clock", text: "Hai risparmiato €10", achieved: true)
-                    ProgressRow(imageName: "clock", text: "Hai risparmiato €20", achieved: false)
-                    ProgressRow(imageName: "clock", text: "Hai risparmiato €50", achieved: false)
-                    ProgressRow(imageName: "clock", text: "Hai risparmiato €100", achieved: false)
+                    ProgressRow(imageName: "clock", text: "Non ho fumato per 1 giorno", achieved: true)
+                    ProgressRow(imageName: "clock", text: "Non ho fumato per 2 giorni", achieved: true)
+                    ProgressRow(imageName: "clock", text: "Non ho fumato per 3 giorni", achieved: true)
+                    ProgressRow(imageName: "clock", text: "Non ho fumato per 4 giorni", achieved: true)
+                    ProgressRow(imageName: "clock", text: "Non ho fumato per 5 giorni", achieved: true)
+                    ProgressRow(imageName: "clock", text: "Non ho fumato per 6 giorni", achieved: true)
+                    ProgressRow(imageName: "clock", text: "Non ho fumato per 1 settimana", achieved: true)
+                    ProgressRow(imageName: "clock", text: "Non ho fumato per 2 settimane", achieved: true)
+                    ProgressRow(imageName: "clock", text: "Non ho fumato per 3 settimane", achieved: true)
+                    ProgressRow(imageName: "clock", text: "Non ho fumato per 4 settimane", achieved: true)
+                    ProgressRow(imageName: "clock", text: "Non ho fumato per 1 mese", achieved: true)
+                    ProgressRow(imageName: "eurosign.circle", text: "Ho risparmiato €10", achieved: true)
+                    ProgressRow(imageName: "eurosign.circle", text: "Ho risparmiato €20", achieved: false)
+                    ProgressRow(imageName: "eurosign.circle", text: "Ho risparmiato €50", achieved: false)
+                    ProgressRow(imageName: "eurosign.circle", text: "Ho risparmiato €100", achieved: false)
+                    
                 }
                 .listStyle(PlainListStyle())
                 .padding(.horizontal, 20)
