aeb0334eb6719c6cbc87240ef98e3ea4708f262f
merola02 <a.merola31@studenti.unisa.it>
2024-07-18 16:18:09 +0200

    n

--- sadb/sadb/sadbApp.swift
+++ sadb/sadb/sadbApp.swift
@@ -13,13 +13,23 @@ struct sadbApp: App {
     
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
-    @AppStorage("hasCompletedQuiz") private var hasCompletedQuiz: Bool = false
-    @AppStorage("hasRegistered") private var hasRegistered: Bool = false
+    //@AppStorage("hasCompletedQuiz") private var hasCompletedQuiz: Bool = false
+    //@AppStorage("hasRegistered") private var hasRegistered: Bool = false
     @AppStorage("isLogged") private var isLogged: Bool = false
     
     var body: some Scene {
         WindowGroup {
+<<<<<<< HEAD
+            
+            if (!isLogged){
+                LoginView()
+            } else {
+                ContentView()
+            }
+            
+=======
             DiaryView()
+>>>>>>> ff8acc1be219510e95545b2d00acd6cd4bbbcce6
         }
     }
 }
@@ -31,3 +41,4 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     return true
   }
 }
+
